98d7ebda1122d67f038324ed71ae38e4897d8cbf
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2022-09-13 01:54:00 +0300

    Fixed Bugs

--- backend/password-api.php
+++ backend/password-api.php
@@ -1,15 +1,11 @@
 <?php
     $password = $_POST["a"];
 
-    if (preg_match("/[A-Z]/", $password) && preg_match("/[a-z]/", $password)) {
-        if (preg_match("/\d/", $password) && preg_match("/\W/", $password)) {
-            if (strlen($password) > 12) {
-                echo "Strong Password";
-            };
+    if (preg_match("/[A-Z]/", $password) && preg_match("/[a-z]/", $password)
+    && preg_match("/\d/", $password) && preg_match("/\W/", $password)
+    && (strlen($password) > 12)) {
+            echo "Strong Password";
         } else {
             echo "Weak Password";
         };
-    } else {
-        echo "Weak Password";
-    };
 ?>
